18fc152f09a826b905a1693426cf3e05b7453d24
Palomero96 <31955373+Palomero96@users.noreply.github.com>
2018-11-04 14:33:11 +0100

    Plan
    Plan para registrar un jugador. Falta matizar la respuesta (agree y refuse)

---
+++ Tablero/TableroRegistraJugadorPlan.java
@@ -0,0 +1,38 @@
+package Tablero
+
+import jadex.adapter.fipa.*;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+
+/**
+ *  PLan para unir a un jugador a la partida
+ */
+public class TableroRegistraJugadorPlan extends Plan
+{
+	
+	public void body()
+	{
+		IMessageEvent	request	= (IMessageEvent)getInitialEvent();
+
+		// ¿que hace el getContent? puede no enviar nada el jugador?
+		Unirse_a_la_Partida rj = (Unirse_a_la_Partida)request.getContent();
+		Jugador player = rj.getJugador();
+		player.setAgentID((AgentIdentifier)request.getParameter("sender").getValue());
+		getLogger().info("New player "+player);
+		if (getBeliefbase.getBeliefSet("jugadores").length < 10) {
+			if(getBeliefbase().getBeliefSet("jugadores")==null){
+				//Si el jugador es el primero que se une a la partida se le asigna como lider
+				player.setLider(True)
+			}
+			if(!getBeliefbase().getBeliefSet("jugadores").containsFact(player))
+			{
+				getBeliefbase().getBeliefSet("jugadores").addFact(player);
+			}
+			player.setEspia(False);
+			player.setLider(False);
+		}
+		//Que haye mandar de respuesta?
+		Done done = new Done(rj);
+		sendMessage(request.createReply("inform_action_done", done));
+	}
+}

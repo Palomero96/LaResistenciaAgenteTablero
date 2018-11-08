e67cc9c551579af85017d6508f9eafd737592c15
David Palomero Méndez <31955373+Palomero96@users.noreply.github.com>
2018-11-08 14:11:45 +0100

    cambios en la distribucion de carpetas

--- Tablero/Planes/TableroRegistraJugadorPlan.java
+++ Tablero/Planes/TableroRegistraJugadorPlan.java
@@ -14,25 +14,28 @@ public class TableroRegistraJugadorPlan extends Plan
 	{
 		IMessageEvent	request	= (IMessageEvent)getInitialEvent();
 
-		// ¿que hace el getContent? puede no enviar nada el jugador?
+		
 		Unirse_a_la_Partida rj = (Unirse_a_la_Partida)request.getContent();
 		Jugador player = rj.getJugador();
 		player.setAgentID((AgentIdentifier)request.getParameter("sender").getValue());
 		getLogger().info("New player "+player);
-		if (getBeliefbase.getBeliefSet("jugadores").length < 10) {
+		if (getBeliefbase.getBeliefSet("jugadores").length < 8) {
+			player.setLider(False);
 			if(getBeliefbase().getBeliefSet("jugadores")==null){
 				//Si el jugador es el primero que se une a la partida se le asigna como lider
 				player.setLider(True)
 			}
+			player.setEspia(False);
+			
 			if(!getBeliefbase().getBeliefSet("jugadores").containsFact(player))
 			{
 				getBeliefbase().getBeliefSet("jugadores").addFact(player);
 			}
-			player.setEspia(False);
-			player.setLider(False);
-		}
+			
 		//Creamos la respuesta para enviar al usuario/jugador
 		Unirse_a_la_Partida rj = new Unirse_a_la_Partida(rj);
 		sendMessage(request.createReply("agree_unirse_a_partida", rj));
 	}
+	Unirse_a_la_Partida rj = new Unirse_a_la_Partida(rj);
+	sendMessage(request.createReply("refuse_unirse_a_partida", rj));
 }

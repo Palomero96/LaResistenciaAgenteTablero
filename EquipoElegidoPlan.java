479851fe740fe68edb628a86fcdb3a9b34f824f4
kirbecio <37543345+kirbecio@users.noreply.github.com>
2018-11-21 13:51:20 +0100

    comentado EquipoElegidoPlan

---
+++ EquipoElegidoPlan.java
@@ -0,0 +1,24 @@
+package tablero.planes;
+import jadex.adapter.fipa.*;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import ontologia.predicados.Equipo_elegido;
+
+/**	Acabado
+ *  Plan para conocer el posible equipo que ira a la mision
+ */
+public class EquipoElegidoPlan extends Plan
+{
+	
+	public void body()
+	{	
+		IMessageEvent inform = (IMessageEvent)getInitialEvent();
+		Equipo_elegido rj = (Equipo_elegido) inform.getContent();
+
+	/* Se informa del equipo planteado para la mision */
+	getBeliefbase().getBelief("Equipo").setFact(rj.getLista_jugadores());
+	
+	/* Se informa de que ya se puede votar para ver si el equipo se aprueba o no */
+	getBeliefbase().getBelief("ActivarAprobarEquipo").setFact(true);
+	}
+}
